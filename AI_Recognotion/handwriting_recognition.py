585e095a6e0a3dc4996651ff02facb1fe182efdd
RipudamanKaushikDal <Ripudaman.kaushik@dal.ca>
2019-07-24 12:59:10 -0300

    downloaded data and ran the program

--- AI_Recognotion/handwriting_recognition.py
+++
@@ -1,6 +0,0 @@
-class Network():
-	def _init_(self,sizes):
-		self.num_layers=len(sizes)
-		self.sizes = sizes
-		self.biases = [np.random.randn(y,1) for y in sizes[1:]]
-		self.weights = [np.random.randn(y,x) for x,y in zip (sizes[:-1],sizes[1:])]
