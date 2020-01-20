f421cf871bef31ecf62c3ba5e5803874fb735c93
RipudamanKaushikDal <Ripudaman.kaushik@dal.ca>
2020-01-19 20:39:05 -0400

    Added layout to main screen and started facial recognition

--- face_recognition/GUI/gui.py
+++ face_recognition/GUI/gui.py
@@ -6,6 +6,7 @@ import cv2
 import os
 import imutils
 import kivy
+import sqlite3
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.label import Label
@@ -36,6 +37,20 @@ class DataEncode(Screen):
 
 	def Pops(self,*args):
 		Pop().open()
+
+	def create_database(self,*args):
+		connection=sqlite3.connect("appdata.db")
+		cursor=connection.cursor()
+
+		command="""
+		CREATE TABLE IF NOT EXISTS info ( dataset text,
+						encodings text, 
+						e-mail text)"""
+		cursor.execute(command)
+		connection.commit()
+		connection.close()
+
+
 
 	def encode(self,dataset,encodings_path,detectionmethod):
 		# grab the paths to the input images in our dataset
@@ -93,7 +108,122 @@ class Pop(Popup):
 
 
 class MainWindow(Screen):
-	pass
+
+	model=ObjectProperty(None)
+	label1=ObjectProperty(None)
+	label2=ObjectProperty(None)
+	check1=ObjectProperty(None)
+	
+
+	def cb_active(self,cbinstance,value):
+		if value:
+			self.model=self.label1.text
+		else:
+			self.model=self.label2.text
+
+	def find_encodings(self):
+		path=os.getcwd()
+		files=os.listdir(path)
+		for i in files:
+			if ".pickle" in i:
+				encoded_file=i
+		return encoded_file
+
+	def capture(self,encoding_file,detection_method):
+		# load the known faces and embeddings
+		print("[INFO] loading encodings...")
+		data = pickle.loads(open(encoding_file, "rb").read())
+		names=[]
+		# initialize the video stream and pointer to output video file, then
+		# allow the camera sensor to warm up
+		print("[INFO] starting video stream...")
+		vs = cv2.VideoCapture(0)
+		# grab the frame from the threaded video stream
+		s,frame = vs.read()
+		if s:
+
+			# convert the input frame from BGR to RGB then resize it to have
+			# a width of 750px (to speedup processing)
+			rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+			# detect the (x, y)-coordinates of the bounding boxes
+			# corresponding to each face in the input frame, then compute
+			# the facial embeddings for each face
+			boxes=face_recognition.face_locations(rgb,model=detection_method)
+			encodings=face_recognition.face_encodings(rgb,boxes)
+			
+				# loop over the facial embeddings
+			for encoding in encodings:
+				# attempt to match each face in the input image to our known
+				# encodings
+				matches = face_recognition.compare_faces(data["encodings"],
+					encoding)
+				name = "Unknown"
+					# check to see if we have found a match
+				if True in matches:
+					# find the indexes of all matched faces then initialize a
+					# dictionary to count the total number of times each face
+					# was matched
+					matchedIdxs = [i for (i, b) in enumerate(matches) if b]
+					counts = {}
+		 
+					# loop over the matched indexes and maintain a count for
+					# each recognized face face
+					for i in matchedIdxs:
+						name = data["names"][i]
+						counts[name] = counts.get(name, 0) + 1
+		 
+					# determine the recognized face with the largest number
+					# of votes (note: in the event of an unlikely tie Python
+					# will select first entry in the dictionary)
+					name = max(counts, key=counts.get)
+				
+				# update the list of names
+				names.append(name)
+			# loop over the recognized faces
+			for ((top, right, bottom, left), name) in zip(boxes, names):
+				# draw the predicted face name on the image
+				cv2.rectangle(frame, (left, top), (right, bottom),
+					(0, 255, 0), 2)
+				y = top - 15 if top - 15 > 15 else top + 15
+				cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
+					0.75, (0, 255, 0), 2)
+
+			# display output frame on screen
+			
+			cv2.imshow("Frame", frame)
+			cv2.waitKey(0)
+			# do a bit of cleanup
+			cv2.destroyAllWindows()
+			vs.release()
+		return names
+
+	def attendance(self):
+		#check to see the encodings_file
+		encoding_data=self.find_encodings()
+		while True:
+			person=self.capture(encoding_data,self.model)
+			
+			if person !=[]:
+				#add name to database
+				if len(person)>1:
+					attn=open("records.txt","w+")
+					for j in names:
+						attn.write("%s %s\n%s %s\n"%("Name:",j,"Attendence:","Present"))
+					print("All Done!")
+					attn.close()
+					break
+				else:
+					attn=open("records.txt","w+")
+					attn.write("%s %s\n%s %s\n"%("Name:",person[0],"Attendence:","Present"))
+					attn.close()
+					print("All Done!")
+					break
+				
+			else:
+				print("Face not found")
+				pass
+
 
 
 class WindowManager(ScreenManager):
@@ -105,7 +235,7 @@ sm=WindowManager()
 screens= [DataEncode(name="encodes"), MainWindow(name="main")]
 for screen in screens:
 	sm.add_widget(screen)
-sm.current="encodes"
+sm.current="main"
 
 class MyApp(App):
 	def build(self):
