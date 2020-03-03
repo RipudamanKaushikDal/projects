7de04de591666f7eb7add2f30e47f627a18c7462
RipudamanKaushikDal <Ripudaman.kaushik@dal.ca>
2020-03-02 22:17:37 -0400

    Started first django test reddit

---
+++ Web Development/Django/personal_portfolio/boards/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Board
+# Create your views here.
+
+def home(request):
+	boards=Board.objects.all()
+	board_names=list()
+	
+	for board in boards:
+		board_names.append(board.name)
+	
+	response_html='<br>'.join(board_names)
+	return HttpResponse(response_html)
