6a06b9b4cd6a12483b87db4cf1244f63d61e1e5d
RipudamanKaushikDal <Ripudaman.kaushik@dal.ca>
2020-03-09 00:28:40 -0300

    Added navigation bar to website and hyperlinks for boards

--- Web Development/Django/personal_portfolio/personal_portfolio/urls.py
+++ Web Development/Django/personal_portfolio/personal_portfolio/urls.py
@@ -19,5 +19,7 @@ from boards import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('boards/<int:pk>/',views.board_topics,name="board_topics"),
     path("", views.home, name="home")
+   
 ]
