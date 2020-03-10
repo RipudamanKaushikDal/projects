ed02b2a7cdcae1020c9341643a4d23898c91bf41
RipudamanKaushikDal <Ripudaman.kaushik@dal.ca>
2020-03-10 17:34:25 -0300

    Added basic view for new topics and added button for navigation

--- Web Development/Django/personal_portfolio/boards/views.py
+++ Web Development/Django/personal_portfolio/boards/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from .models import Board
-from django.http import Http404
+from django.shortcuts import render,redirect,get_object_or_404
+from django.contrib.auth.models import User
+from .models import Board,Topics,Posts
+
 # Create your views here.
 
 def home(request):
@@ -8,15 +9,26 @@ def home(request):
 	return render(request,"home.html",{"boards":boards})
 
 def board_topics(request,pk):
-	try:
-		board=Board.objects.get(pk=pk)
-	except Board.DoesNotExist:
-        	raise Http404
+	board=get_object_or_404(Board,pk=pk)
 	return render(request,"topics.html",{"board":board})
 
 def new_topic(request,pk):
-	try:
-		board=Board.objects.get(pk=pk)
-	except Board.DoesNotExist:
-        	raise Http404
+	board=get_object_or_404(Board,pk=pk)
+	if request.method=="POST":
+		subject=request.POST["subject"]
+		message=request.POST["message"]
+	
+		user=User.objects.first()
+
+		topic=Topics.objects.create(
+			subject=subject,
+			board=board,
+			starter=user)
+
+		post=Posts.objects.create(
+			message=message,
+			topic=topic,
+			created_by=user)
+		return redirect('board_topics',pk=board.pk)
+
 	return render(request,"new_topic.html",{"board":board})
