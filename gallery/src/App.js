b23985ffd5004c9084deba2774f73231ff9e7ae2
RipudamanKaushikDal <Ripudaman.kaushik@dal.ca>
2020-05-11 04:28:10 -0300

    Started building gallery app and finished watch drone on react

---
+++ gallery/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <header>
+      <h1>Image Search Gallery</h1>
+    </header>
+      
+  );
+}
+
+export default App;
