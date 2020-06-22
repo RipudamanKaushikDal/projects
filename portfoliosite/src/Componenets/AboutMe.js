98ff18798e9420e308bbb1d129fba670901ecdeb
RipudamanKaushikDal <Ripudaman.kaushik@dal.ca>
2020-06-22 02:36:49 -0300

    Finished Designing AboutMe page

--- portfoliosite/src/Componenets/AboutMe.js
+++ portfoliosite/src/Componenets/AboutMe.js
@@ -2,11 +2,16 @@ import React from 'react'
 import Chip from '@material-ui/core/Chip';
 import CodeRoundedIcon from '@material-ui/icons/CodeRounded';
 import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography'
+import me from '../images/me.jpg';
 
 
 const useStyles = makeStyles((theme) => ({
     skills: {
-        paddingTop: 320,
         display: 'flex',
         justifyContent: 'center',
         flexWrap: 'wrap',
@@ -20,8 +25,27 @@ function AboutMe(){
 
     const classes = useStyles();
     return(
-        <>
-            <h1>About ME</h1>
+        <div className="bio">
+            <Card style={{maxWidth:360,marginTop:15,alignSelf:'center'}}>
+                  <CardActionArea>
+                      <CardMedia 
+                        style={{height:300}}
+                        image={me}
+                        title="Dev"
+                        />
+                      <CardContent>
+                        <Typography gutterBottom variant="h5" component="h2">
+                            Ripudaman Kaushik
+                        </Typography>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                            I am a full-stack developer who loves to code. On a constant lookout for fresh ideas
+                            and new technologies to learn. My mantra "Eager to Start, Determined to Finish."
+                        </Typography>
+                      </CardContent>
+                  </CardActionArea>
+            </Card>
+            <h1>Skills</h1>
+            
             <div className={classes.skills}>
                 <Chip
                 variant="default"
@@ -94,7 +118,7 @@ function AboutMe(){
 
 
         
-        </>
+        </div>
     )
 }
 
