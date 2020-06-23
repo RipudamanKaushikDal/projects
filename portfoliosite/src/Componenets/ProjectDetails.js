dc50c678b767dee404debc9d41f6bd4eea761f03
RipudamanKaushikDal <Ripudaman.kaushik@dal.ca>
2020-06-23 03:20:03 -0300

    Designed the projects page

---
+++ portfoliosite/src/Componenets/ProjectDetails.js
@@ -0,0 +1,22 @@
+/*import React from 'react';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import MobileStepper from '@material-ui/core/MobileStepper';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+
+
+function importAll(r) {
+    let images = {};
+    r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+    return images;
+  }
+  
+  const images = importAll(require.context('../images/projectimages/', false, /\.(png|jpe?g|svg)$/));
+
+
+const projectStuff ={
+    faceApp: 
+        images=[]
+} */
--- portfoliosite/src/Componenets/Projects.js
+++ portfoliosite/src/Componenets/Projects.js
@@ -1,9 +1,108 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button';
+import CardActions  from '@material-ui/core/CardActions';
+import card1 from '../images/project card1.png'
+import card2 from '../images/project card2.png'
+import card3 from '../images/project card3.jpg'
+
+/*const [state,setState]=useState({
+    index:null
+})
+
+const chooseView=(number) => {
+    setState=() => {return{index:number}}
+
+}*/
 
 function Projects(){
     return(
         <>
-            <h1>Projects</h1>
+         <div className="projects">
+            <Card style={{maxWidth:300,marginTop:15}}  >
+                  <CardActionArea>
+                      <CardMedia 
+                        style={{height:200}}
+                        image={card1}
+                        />
+                      <CardContent>
+                        <Typography gutterBottom variant="h5" component="h2">
+                            Image Search Gallery
+                        </Typography>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                            A WebApp that makes use of AI to find similar images in a sea fo images.
+                            The AI algorithms are deployed in a self-made Web-server and the whole app is hosted in Docker
+                        </Typography>
+                      </CardContent>
+                  </CardActionArea>
+                  <CardActions>
+                    <Button size="medium" color="secondary">
+                        Github
+                    </Button>
+                    <Button size="medium" color="secondary">
+                        LinkedIn
+                    </Button>
+                  </CardActions>
+            </Card>
+
+            <Card style={{maxWidth:300,marginTop:15,marginLeft:60}}  >
+                  <CardActionArea>
+                      <CardMedia 
+                        style={{height:200}}
+                        image={card2}
+                        />
+                      <CardContent>
+                        <Typography gutterBottom variant="h5" component="h2">
+                            Automated Attendance App
+                        </Typography>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                            App to recognize students in a class and mark their attendance.
+                            It also forwards the daily attendance record to a specified professor's mail.
+                        </Typography>
+                      </CardContent>
+                  </CardActionArea>
+                  <CardActions>
+                    <Button size="medium" color="secondary">
+                        Github
+                    </Button>
+                    <Button size="medium" color="secondary">
+                        LinkedIn
+                    </Button>
+                  </CardActions>
+            </Card>
+
+            <Card style={{maxWidth:300,marginTop:30}}  >
+                  <CardActionArea>
+                      <CardMedia 
+                        style={{height:200}}
+                        image={card3}
+                        />
+                      <CardContent>
+                        <Typography gutterBottom variant="h5" component="h2">
+                            Watch Drone
+                        </Typography>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                            A WebApp designed to help people to search their favourite tv-shows and movies.
+                            It also lets them watch their trailers on Youtube. 
+                        </Typography>
+                      </CardContent>
+                  </CardActionArea>
+                  <CardActions>
+                    <Button size="medium" color="secondary">
+                        Github
+                    </Button>
+                    <Button size="medium" color="secondary">
+                        LinkedIn
+                    </Button>
+                  </CardActions>
+            </Card>
+
+
+          </div>
             
 
 
